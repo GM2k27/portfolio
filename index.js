e43a6c2616b4893876c2f6497d61b9a75f75ecac
GM2k27 <vaishnavigm0@gmail.com>
2025-11-01 00:33:53 +0530

    Modified form section

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-$(document).ready(function(){
-const arr = [' Full-stack developer',' Cybersecurity Enthusiast'];
-let ind=0;
+$(document).ready(function () {
+    const arr = [' Full-stack developer', ' Cybersecurity Enthusiast'];
+    let ind = 0;
 
-function typingText(){
-    $(".type").fadeOut(1000,function(){
-        $(this).text(arr[ind]).fadeIn(1000);
-    })
+    function typingText() {
+        $(".type").fadeOut(1000, function () {
+            $(this).text(arr[ind]).fadeIn(1000);
+        })
 
-    ind=(ind+1)%arr.length;
-}
+        ind = (ind + 1) % arr.length;
+    }
 
-typingText();
-setInterval(typingText,2000)
+    typingText();
+    setInterval(typingText, 2000);
+
+    // Contact form submission handler
+    $('#contactForm').on('submit', function (e) {
+        e.preventDefault();
+        var form = $(this);
+        var formData = form.serialize();
+
+        $.ajax({
+            url: form.attr('action'),
+            method: 'POST',
+            data: formData,
+            dataType: 'json',
+            success: function (response) {
+                form.hide();
+                $('#thankYouMessage').removeClass('hidden');
+            },
+            error: function (xhr, status, error) {
+                alert('Sorry, there was an error sending your message. Please try again.');
+            }
+        });
+    });
+
+    // Go back link handler
+    $('#goBackLink').on('click', function (e) {
+        e.preventDefault();
+        $('#thankYouMessage').addClass('hidden');
+        $('#contactForm')[0].reset();
+        $('#contactForm').show();
+    });
 })
 
